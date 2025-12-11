36ed7233ba15238a6598dd1703fa77887c331513
Witold Strzeboński <wstrzebo@gmail.com>
2025-12-11 20:27:33 +0100

    add p2 production

--- productions/__init__.py
+++ productions/__init__.py
@@ -2,6 +2,7 @@
 
 from productions.production_base import Production
 from productions.p0.p0 import P0
+from productions.p2.p2 import P2
 from productions.p6.p6 import P6
 
-__all__ = ['Production', 'P0', 'P6']
+__all__ = ['Production', 'P0', 'P2', 'P6']

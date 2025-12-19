f50f32522f730b0e1aa11b213892e49014bb52f1
wlpinjlte <wlpinjlte@gmail.com>
2025-12-19 09:11:50 +0100

    add p6 example in large graph

--- productions/p6/example.py
+++ productions/p6/example.py
@@ -34,3 +34,35 @@ if can_apply:
     production.apply(graph, matched)
 
 graph.visualize(os.path.join(output_dir, "after_p6.png"))
+
+
+graph = HyperGraph()
+coords = [(1, 1), (1, 6), (5, 9), (9, 6), (9, 1)]
+nodes = [graph.add_node(x, y) for (x, y) in coords]
+
+edges = []
+for i in range(5):
+    n1 = nodes[i]
+    n2 = nodes[(i + 1) % 5]
+    border = i < 2 or i == 4
+    e = graph.add_edge(n1, n2, is_border=border)
+    edges.append(e)
+p = graph.add_hyperedge(nodes, label="P")
+n1 = graph.add_node(14, 1)
+n2 = graph.add_node(14, 6)
+n3 = graph.add_node(14, 9)
+graph.add_edge(nodes[4], n1, is_border=True)
+graph.add_edge(nodes[3], n2, is_border=False)
+graph.add_edge(nodes[2], n3, is_border=True)
+graph.add_edge(n1, n2, is_border=True)
+graph.add_edge(n2, n3, is_border=True)
+p = graph.add_hyperedge([nodes[4], nodes[3], n1, n2], label="P")
+p = graph.add_hyperedge([nodes[2], nodes[3], n3, n2], label="P")
+
+graph.visualize(os.path.join(output_dir, "before_p6_larger_graph_before.png"))
+
+can_apply, matched = production.can_apply(graph)
+if can_apply:
+    production.apply(graph, matched)
+
+graph.visualize(os.path.join(output_dir, "after_p6_larger_graph_after.png"))
